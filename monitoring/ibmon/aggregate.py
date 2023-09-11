45926646c29d1d0bb7f89876b5e4425e14bd1aaf
Caleb Carlson <ccarlson355@gmail.com>
2023-09-11 11:02:05 -0600

    Add proper logging to aggregate.py

--- monitoring/ibmon/aggregate.py
+++ monitoring/ibmon/aggregate.py
@@ -1,5 +1,6 @@
 #!/bin/python3
 
+import logging as log
 import os
 import pandas as pd
 from pprint import pprint
@@ -7,7 +8,8 @@ import sys
 
 
 def usage():
-    print("Usage:\n\taggregate.py <directory> <monitor_id>\n")
+    print("Usage:\n\taggregate.py <directory> <monitor_id> [OPTIONS]\n")
+    print("OPTIONS:\n\t--log-level\tDEBUG,INFO,WARNING,ERROR\n")
 
 
 def find_csv_files(directory: str, monitor_id: str) -> dict:
@@ -30,8 +32,8 @@ def find_csv_files(directory: str, monitor_id: str) -> dict:
                  and monitor_id in x and "aggregate" not in x]
 
     if len(csv_files) == 0:
-        print(f"Did not find any csv files in {directory} with monitor "
-              f"id {monitor_id}")
+        log.error(f"Did not find any csv files in {directory} with monitor "
+                  f"id {monitor_id}")
         return {}
 
     # strip off trailing '/' from directory if it exists
@@ -70,17 +72,17 @@ def aggregate_results(csv_files: dict, monitor_id: str, out_dir: str) -> None:
         for csv_file in csv_list:
 
             # Retrieve the device name from filename
-            print(f"DEBUG: csv_file: {csv_file}")
+            log.debug(f"csv_file: {csv_file}")
             basename = os.path.basename(csv_file)
             fields = basename.split("_")  # ["n01", "mlx5", "3", ...]
             device = f"{fields[1]}_{fields[2]}"
-            print(f"DEBUG: device name: {device}")
+            log.debug(f"device name: {device}")
 
             df = pd.read_csv(csv_file, header=0)
             cols = len(df.axes[1])
             if cols < 6:
-                print(f"Found CSV file ({csv_file}) with only {cols} columns."
-                      f" Skipping.")
+                log.debug(f"Found CSV file ({csv_file}) with only {cols} "
+                          f"columns. Skipping.")
                 continue
 
             # Rename all non-timestamp columns to append device name as suffix
@@ -93,7 +95,7 @@ def aggregate_results(csv_files: dict, monitor_id: str, out_dir: str) -> None:
 
             individual_dfs.append(df)
 
-        print(f"DEBUG: Set of unique columns: {unique_column_keys}")
+        log.debug(f"Set of unique columns: {unique_column_keys}")
 
         # Merge all DFs together, so we have a column per device
         left_df = individual_dfs[0]
@@ -106,7 +108,7 @@ def aggregate_results(csv_files: dict, monitor_id: str, out_dir: str) -> None:
         # to aggregate all columns with each key name in it.
         for key in unique_column_keys:
             columns_sharing_key = [x for x in left_df.columns if key in x]
-            print(f"DEBUG: columns sharing key {key}: {columns_sharing_key}")
+            log.debug(f"columns sharing key {key}: {columns_sharing_key}")
 
             # Sum all columns sharing this common key and add as new column
             left_df[f"{key}_sum"] = left_df[columns_sharing_key].sum(axis=1)
@@ -114,18 +116,40 @@ def aggregate_results(csv_files: dict, monitor_id: str, out_dir: str) -> None:
         # Save DataFrame as aggregate CSV file
         out_filename = f"{hostname}_{monitor_id}_aggregate.csv"
         out_full_path = os.path.join(out_dir, out_filename)
-        print(f"Saving {out_full_path}")
+        log.info(f"Saving {out_full_path}")
         left_df.to_csv(out_full_path, sep=",", header=True, index=False)
 
 
 def main():
-    if len(sys.argv) != 3:
+    if 3 > len(sys.argv) > 4:
         usage()
         exit(1)
 
+    log_level = log.DEBUG
+    if len(sys.argv) == 4:
+        string_arg = sys.argv[3]
+        if "--log-level=" not in string_arg:
+            usage()
+            exit(1)
+        value = string_arg.split("=")[1]
+        if value == "DEBUG":
+            log_level = log.DEBUG
+        elif value == "INFO":
+            log_level = log.INFO
+        elif value == "WARNING":
+            log_level = log.WARNING
+        elif value == "ERROR":
+            log_level = log.WARNING
+        else:
+            print("Invalid --log-level value.")
+            usage()
+            exit(1)
+
+    log.basicConfig(level=log_level)
+
     results_dir = sys.argv[1]
     monitor_id = sys.argv[2]
-    print(f"DEBUG: results_dir='{results_dir}', monitor_id='{monitor_id}'")
+    log.debug(f"results_dir='{results_dir}', monitor_id='{monitor_id}'")
     csv_files = find_csv_files(results_dir, monitor_id)
     pprint(csv_files)
 
@@ -133,7 +157,6 @@ def main():
         exit(1)
 
     aggregate_results(csv_files, monitor_id, results_dir)
-
 
 
 if __name__ == '__main__':
